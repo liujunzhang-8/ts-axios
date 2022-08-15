efe7b15c1377e5fcfee9c6b8d823979a9d1e314b
Gorgio_Liu666 <15711097909@163.com>
2022-08-16 00:24:15 +0800

    ts-axios

--- typescript-library-starter/src/types/index.ts
+++ typescript-library-starter/src/types/index.ts
@@ -11,4 +11,18 @@ export interface AxiosRequestConfig {
   method?: Method
   data?: any
   params?: any
+  headers?: any
+  responseType?: XMLHttpRequestResponseType
+  timeout?: number
 }
+
+export interface AxiosResponse<T = any> {
+  data: T
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> { }
